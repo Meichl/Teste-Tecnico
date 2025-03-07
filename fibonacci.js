475e125c70b73e43bc85d9ce7cdadcf8dfe50f69
Michel Gerônimo <105396013+Meichl@users.noreply.github.com>
2025-03-07 17:18:15 -0300

    Create fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,17 @@
+function isFibonacci(num) {
+    let a = 0, b = 1;
+    while (b < num) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b === num || num === 0;
+}
+
+const numeroInformado = 21; // Número informado
+
+if (isFibonacci(numeroInformado)) {
+    console.log(`${numeroInformado} pertence à sequência de Fibonacci.`);
+} else {
+    console.log(`${numeroInformado} não pertence à sequência de Fibonacci.`);
+}
